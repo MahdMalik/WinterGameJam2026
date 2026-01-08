b595ebcaea836c52084a8c64c2a396d93c4b5125
Cheesedoghotdog <jackhotcheese@gmail.com>
2026-01-08 00:16:41 -0600

    Attempts With Doors
    Messed with the scripts a bunch.

---
+++ Assets/Scripts/Environment/Door.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] Sprite Closed;
+    [SerializeField] Sprite Open;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
