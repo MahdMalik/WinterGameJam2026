98c8909badd018d41516569a609d9cdc82e6d86a
Cheesedoghotdog <jackhotcheese@gmail.com>
2026-01-09 03:13:28 -0600

    More work on Interaction
    Closer to Doors.

--- Assets/Scripts/Environment/Door.cs
+++ Assets/Scripts/Environment/Door.cs
@@ -6,7 +6,11 @@ public class Door : MonoBehaviour
 {
     [SerializeField] Sprite Closed;
     [SerializeField] Sprite Open;
+    [SerializeField] Sprite PartOpen;
+    [SerializeField] Sprite PartClosed;
     [SerializeField] private SpriteRenderer DoorSprite;
+    [SerializeField] BoxCollider2D doorHitbox;
+    [SerializeField] float OpenTime;
 
     // Update is called once per frame
     void Update()
@@ -14,6 +18,19 @@ public class Door : MonoBehaviour
         
     }
     public void OpenDoor() {
-        DoorSprite.sprite = Open;
+        if (doorHitbox.IsTouchingLayers(LayerMask.GetMask("Player"))) {
+            StartCoroutine(OpenTheDoor(OpenTime));
+        }
     }
+
+
+
+ private IEnumerator OpenTheDoor(float time) {
+    DoorSprite.sprite = PartClosed;
+    yield return new WaitForSeconds(time);
+    DoorSprite.sprite = PartOpen;
+    yield return new WaitForSeconds(time);
+    DoorSprite.sprite = Open;
+    yield return new WaitForSeconds(time);
+ }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Sprite Down;
     [SerializeField] Sprite Left;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private GameObject interactSquare;
+    [SerializeField] private BoxCollider2D interactHitbox;
+    [SerializeField] private GameObject Player;
     [SerializeField] private float PlayerSpeed;
     [SerializeField] private int PlayerFacing = 3;
     [SerializeField] private SpriteRenderer PlayerSprite;
@@ -29,19 +32,32 @@ public class PlayerMovement : MonoBehaviour
         }//Right
         if (rb.velocity.x == 0 && rb.velocity.y <= 0) {
             PlayerFacing = 3;
+            
         }//Down
         if (rb.velocity.x < 0) {
             PlayerFacing = 4;
+            
         }//Left
         if(PlayerFacing == 1) {
             PlayerSprite.sprite = Up;
+            interactSquare.transform.position = new Vector3(Player.transform.position.x, (Player.transform.position.y + 1.0f), Player.transform.position.z);
         } else if(PlayerFacing == 2) {
             PlayerSprite.sprite = Right;
+            interactSquare.transform.position = new Vector3((Player.transform.position.x + 1.0f), Player.transform.position.y, Player.transform.position.z);
         } else if(PlayerFacing == 3) {
             PlayerSprite.sprite = Down;
+            interactSquare.transform.position = new Vector3(Player.transform.position.x, (Player.transform.position.y - 1.0f), Player.transform.position.z);
         } else {
             PlayerSprite.sprite = Left;
+            interactSquare.transform.position = new Vector3((Player.transform.position.x - 1.0f), Player.transform.position.y, Player.transform.position.z);
         }
-        Debug.Log(PlayerFacing);
+
+
+
+        if(Input.GetKeyDown(KeyCode.Z)) {
+            if (interactHitbox.IsTouchingLayers(LayerMask.GetMask("Door"))) {
+                
+            }
+        }
     }
 }
