b3d57a9d7518ed81646f76838c241ccb211b15fc
MahdMalik <mahdmahdmalik@gmail.com>
2026-01-11 04:36:35 -0600

    progress thus far but i ain't done

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    private int chosenItemIndex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        chosenItemIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/System/ItemUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemUI : MonoBehaviour
+{
+    Item[] inventory;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = new Item[4];
+    }
+
+    // Update is called once per frcame
+    void Update()
+    {
+        
+    }
+}
