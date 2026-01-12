643402dae5ae0e6b904da72395011cb214c484b3
Cheesedoghotdog <jackhotcheese@gmail.com>
2026-01-11 22:28:23 -0600

    Slash Fully Implemented!
    Objectusespeed changes how fast you can slash.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,8 +14,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private GameObject interactSquare;
     [SerializeField] private BoxCollider2D interactHitbox;
     [SerializeField] private float PlayerSpeed;
+    [SerializeField] private float objectUseSpeed;
+    [SerializeField] private bool canAct = true;
+    [SerializeField] private bool canTurnInteract = true;
     [SerializeField] private SpriteRenderer PlayerSprite;
     [SerializeField] Animator playerAnim;
+    [SerializeField] Animator slashAnim;
 
     public static Action ResetGame;
 
@@ -37,14 +41,29 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Z)) {
             if (interactHitbox.IsTouchingLayers(LayerMask.GetMask("Door"))) {
                 OpeningDoor = true;
+            } else  if (canAct) {
+            canAct = false;
+            canTurnInteract = false;
+            if (PlayerVars.PlayerFacing == 1) {
+                slashAnim.SetInteger("SlashDirection", 1);
+            } else if (PlayerVars.PlayerFacing == 2) {
+                slashAnim.SetInteger("SlashDirection", 2);
+            } else if (PlayerVars.PlayerFacing == 3) {
+                slashAnim.SetInteger("SlashDirection", 3);
+            } else {
+                slashAnim.SetInteger("SlashDirection", 4);
             }
+            slashAnim.SetTrigger("Slashing");
+            StartCoroutine(WaitToAct());
+        }
         }
 
-                //Check if walking
+        //Check if walking
         if (rb.velocity.x == 0 && rb.velocity.y == 0) {
             playerAnim.SetBool("Walking", false);
         } else {
             playerAnim.SetBool("Walking", true);
+        
         //Facing Logic
         if(rb.velocity.x == 0 && rb.velocity.y > 0) {
             PlayerVars.PlayerFacing = 1;
@@ -63,18 +82,18 @@ public class PlayerMovement : MonoBehaviour
             playerAnim.SetInteger("WalkingDirection", 4);
         }//Left
         }
-        if(PlayerVars.PlayerFacing == 1) {
+        if(PlayerVars.PlayerFacing == 1 && canTurnInteract) {
             PlayerSprite.sprite = Up;
-            interactSquare.transform.position = new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z);
-        } else if(PlayerVars.PlayerFacing == 2) {
+            interactSquare.transform.position = new Vector3(transform.position.x, transform.position.y + 1.3f, transform.position.z);
+        } else if(PlayerVars.PlayerFacing == 2 && canTurnInteract) {
             PlayerSprite.sprite = Right;
-            interactSquare.transform.position = new Vector3(transform.position.x + 1.0f, transform.position.y + 0.5f, transform.position.z);
-        } else if(PlayerVars.PlayerFacing == 3) {
+            interactSquare.transform.position = new Vector3(transform.position.x + 0.8f, transform.position.y + 0.5f, transform.position.z);
+        } else if(PlayerVars.PlayerFacing == 3 && canTurnInteract) {
             PlayerSprite.sprite = Down;
-            interactSquare.transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
-        } else {
+            interactSquare.transform.position = new Vector3(transform.position.x, transform.position.y - 0.3f, transform.position.z);
+        } else if(PlayerVars.PlayerFacing == 4 && canTurnInteract) {
             PlayerSprite.sprite = Left;
-            interactSquare.transform.position = new Vector3(transform.position.x - 1.0f, transform.position.y + 0.5f, transform.position.z);
+            interactSquare.transform.position = new Vector3(transform.position.x - 0.8f, transform.position.y + 0.5f, transform.position.z);
         }
     }
 
@@ -83,6 +102,14 @@ public class PlayerMovement : MonoBehaviour
         transform.position = Vector3.zero;
         rb.velocity = Vector2.zero;
         ResetGame.Invoke();
+    }
+
+
+    IEnumerator WaitToAct() {
+        yield return new WaitForSeconds(objectUseSpeed/2);
+        canTurnInteract = true;
+        yield return new WaitForSeconds(objectUseSpeed/2);
+        canAct = true;
     }
 
 
