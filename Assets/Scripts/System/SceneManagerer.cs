93e91099dd197695f1db9af18f4b5e2b41d78eae
Cheesedoghotdog <jackhotcheese@gmail.com>
2026-01-12 00:11:13 -0600

    Quick Fix for Resolution issue.

--- Assets/Scripts/System/SceneManagerer.cs
+++ Assets/Scripts/System/SceneManagerer.cs
@@ -27,7 +27,8 @@ public class SceneManagerer : MonoBehaviour
         }
         MusicManagement = GameObject.Find("MusicManager");
         StartCoroutine(FadeInMusic());
-        
+        Initializer.RT.height = 1920;
+        Initializer.RT.width = 1080;
     }
     
     IEnumerator FadeInMusic() {
@@ -50,9 +51,13 @@ public class SceneManagerer : MonoBehaviour
     private IEnumerator GoToNextScene() {
         Initializer.PixelatedPanel.SetActive(true);
         Initializer.PixelCamera.gameObject.SetActive(true);
-        for (int i = 1; i < 51; i++) {
+        for (int i = 1; i < 41; i++) {
             AdjustRenderTextureSize(i, i);
             yield return new WaitForSeconds(0.03f);
+        }
+        for (int i = 41; i < 71; i++) {
+            AdjustRenderTextureSize((i * i), (i * i));
+            yield return new WaitForSeconds(0.01f);
         }
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
     }
