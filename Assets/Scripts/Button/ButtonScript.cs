3bd48985d3378b09bd6c0145ace143405144b1d5
Mahd Malik <mahdmahdmalik@gmail.com>
2026-01-16 21:49:08 -0600

    clicking da button works

--- Assets/Scripts/Button/ButtonScript.cs
+++ Assets/Scripts/Button/ButtonScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReplayButton : MonoBehaviour
+public class ButtonScript : MonoBehaviour
 {    
     void Awake()
     {
