a5b3613920ce7340b4901d9fb56c185610c22777
Cheesedoghotdog <jackhotcheese@gmail.com>
2026-01-17 19:19:16 -0600

    Enemy Sounds

--- Assets/Scripts/Enemy AI/BasicAI.cs
+++ Assets/Scripts/Enemy AI/BasicAI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BasicAI : MonoBehaviour
 {
@@ -40,6 +41,10 @@ public class BasicAI : MonoBehaviour
     private float knockbackTime;
     public float counterForKnockback = 0.1f;
 
+    public Sound[] SFXSounds;
+    [SerializeField] private AudioSource SFXSource;
+    [SerializeField] private bool seeingSoundCooldown;
+
     void Start()
     {
         // Try to find the player
@@ -63,6 +68,7 @@ public class BasicAI : MonoBehaviour
 
     void Update()
     {
+        SFXSource.volume = Initializer.SFXVolume;
         if(player == null)
             return;
         
@@ -85,6 +91,11 @@ public class BasicAI : MonoBehaviour
             if(distanceToPlayer < aggroRange)
             {
                 MoveTowardsPlayer(distanceToPlayer);
+                if (!seeingSoundCooldown) {
+                    seeingSoundCooldown = true;
+                    StartCoroutine(SeesRepeat());
+                    PlaySFX("Sees");
+                }
             }
         }
         if(hasBeenHit)
@@ -97,6 +108,20 @@ public class BasicAI : MonoBehaviour
                     1);
                 iframeTime = 0;
             }
+        }
+    }
+
+    IEnumerator SeesRepeat() {
+        yield return new WaitForSeconds(1.0f);
+        seeingSoundCooldown = false;
+    }
+    public void PlaySFX(string name) {
+        Sound s = Array.Find(SFXSounds, x => x.name == name);
+        if(s == null) {
+            Debug.Log("No Sounds");
+        } else {
+            SFXSource.clip = s.clip;
+            SFXSource.Play();
         }
     }
 
@@ -279,12 +304,14 @@ public class BasicAI : MonoBehaviour
             if(healthPoints <= 0)
             {
                 Initializer.numKillsThisRound += 1;
+                PlaySFX("Dead");
                 Batteri.GetComponent<Battery>().AlterBattery(5.0f);
                 gameObject.SetActive(false);
             }
             else
             {
                 inKnockback = true;
+                PlaySFX("Hit");
                 knockbackUnitDirection = (transform.position - player.position).normalized * knockBackFactor;
                 GetComponent<SpriteRenderer>().color = new Color(GetComponent<SpriteRenderer>().color.r, GetComponent<SpriteRenderer>().color.g, GetComponent<SpriteRenderer>().color.b,
                     0.5f);
